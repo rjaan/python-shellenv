b2df8ad6d6bae62416be7823ae69a27290c70407
Andrew Rzhavskov <rjaan@yandex.ru>
2022-03-03 21:27:22 +0300

    add function getenv() to search a varible value on the key

--- src/shellenv_module/shellenv.py
+++ src/shellenv_module/shellenv.py
@@ -8,11 +8,21 @@ __copyright__   = "Copyright 2022, Russia Moscow"
 import os
 
 #
-# the function shell_env is an analog of shell-command ENV(1)
+# the function shell_env is an analog of C-function getenv(3)
+# 
+def getenv(keyenv):
+    for key, value in os.environ.items():
+       if keyenv == key : 
+          return value
+    return "" # return an empty text if nothings has been found   
+
+#
+# the function print_all is an analog of shell-command ENV(1)
 # 
 def print_all():
     print ( '*** Print all environment variables ***' )
 # Actually, output the environment variables are accessible in python's script
-    for item, value in os.environ.items():
-       print('{}: {}'.format(item, value))   
- 
+    for key in os.environ:
+       print('{}={}'.format(key, getenv(key)))   
+
+      
